2d453069276f6f75d11a764bdfb44e78030430be
tanyagoyal3110 <tanyastpauls@gmail.com>
2025-11-25 10:05:19 +0530

    updated allowedorigins in cors

--- auth-service/src/main/java/com/example/auth/security/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
         http
                 .cors(cors -> cors.configurationSource(req -> {
                     var config = new org.springframework.web.cors.CorsConfiguration();
-                    config.setAllowedOrigins(List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(List.of("http://localhost:5173","http://34.232.84.229"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setAllowCredentials(true);
